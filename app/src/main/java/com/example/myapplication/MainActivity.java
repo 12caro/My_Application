8cb3b5301d5f35d4beb54dc7049af064f30e7fb9
12caro <30931904+12caro@users.noreply.github.com>
2020-07-19 19:27:39 -0500

    mi primer mensaje

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+ //este es mi primer comentario
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
